fbfcc6d37b05edeccefa0fccee76f2a16273e1fc
Amauri Dias <athlon.amauri@gmail.com>
2023-06-01 23:10:10 -0300

    feat: setup db postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/types/environment.d.ts
@@ -0,0 +1,13 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB_HOST: string;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+      DB_PORT: string;
+      DB_DATABASE: string;
+    }
+  }
+}
